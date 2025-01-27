173f2b22f40520b3edd372ee74a94bf473fdf3e0
Jaeyoung Cho <joeyadamcho@gmail.com>
2025-01-27 14:47:28 +0100

    devcontainer build fix

--- scripts/view-starter-dev.sh
+++ scripts/view-starter-dev.sh
@@ -7,5 +7,5 @@ github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-reveal -> ../..
 github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-seo -> ../../../modules/wowchemy-seo,
 github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-core -> ../../../modules/wowchemy-core,
 github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-netlify-cms -> ../../../modules/wowchemy-plugin-netlify-cms" \
-hugo server --panicOnWarning --minify -F \
+hugo server --bind=0.0.0.0 --port=1313 --panicOnWarning --minify -F \
 --source "starters/$1" --destination "./dist"
